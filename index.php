7735d23326a7ba52d189eb6e0efe7722ac53e78c
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-06-12 16:05:24 +0200

    Add: *vuejs-3 implementation

--- index.php
+++ index.php
@@ -7,14 +7,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- vueJs -->
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <!-- bootStrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- axios -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 <body>
-    
+     <!-- vueJs-3 -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <div id="app">{{ message }}</div>
+
+<script>
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+</script>
 </body>
 </html>
